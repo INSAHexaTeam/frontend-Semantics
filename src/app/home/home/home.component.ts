90d573333173351a5e345d8d1f6e7c274026448b
cramara <gabin.joussot-dubien@insa-lyon.fr>
2024-12-09 14:17:16 +0100

    joli page

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,6 +1,6 @@
 import { SportifService } from './../../_services/sportif.service';
 import { SharedDataService } from './../../_services/shared-data.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { Sportif } from '../../_interfaces/sportif';
 import { SportifCardComponent } from '../sportif-card/sportif-card.component';
 import { Router } from '@angular/router';
@@ -16,6 +16,11 @@ export class HomeComponent implements OnInit {
   sportifs: Sportif[] = [];
   has_list: boolean = false;
   sportifs_display: Sportif[] = [];
+  
+  currentPage: number = 1;
+  itemsPerPage: number = 36;
+  totalPages: number = 0;
+
   constructor(
     private sharedDataService: SharedDataService,
     private SportifService: SportifService,
@@ -23,17 +28,52 @@ export class HomeComponent implements OnInit {
   ) {
     this.SportifService.getAllSportifs().subscribe((s: Sportif[]) => {
       this.sportifs = s;
+      this.updateDisplayedSportifs();
     });
   }
 
   ngOnInit() {
     this.sharedDataService.data$.subscribe((data) => {
       this.sportifs = data;
-      this.sportifs_display = data;
+      this.updateDisplayedSportifs();
       this.has_list = true;
     });
   }
+
+  updateDisplayedSportifs() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.sportifs_display = this.sportifs.slice(startIndex, endIndex);
+    this.totalPages = Math.ceil(this.sportifs.length / this.itemsPerPage);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateDisplayedSportifs();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateDisplayedSportifs();
+    }
+  }
+
   sportifDetails(sportif: Sportif) {
     this.router.navigate(['sportif', sportif.athleteId]);
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    switch(event.key) {
+      case 'ArrowRight':
+        this.nextPage();
+        break;
+      case 'ArrowLeft':
+        this.previousPage();
+        break;
+    }
   }
 }

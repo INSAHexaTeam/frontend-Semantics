5b4fd046ac8130b8390eacb705dd59c22c48484c
Santfogu <santiforerog24@hotmail.com>
2024-12-16 08:28:55 +0100

    Petit fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { SportifDetailsComponent } from './sportif-details/sportif-details.component';
 import { AppComponent } from './home/app.component';
 import { HomeComponent } from './home/home/home.component';
-import { SportDetailsComponent } from './sport-details /sport-details.component';
+import { SportDetailsComponent } from './sport-details/sport-details.component';
 
 export const routes: Routes = [
   { path: 'sportif/:id', component: SportifDetailsComponent },
